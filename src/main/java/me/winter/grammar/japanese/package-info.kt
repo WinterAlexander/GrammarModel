4d791be42e7ec1f6106d6b85b843fb496b027a2d
Alexander Winter <a.w1nter@hotmail.com>
2017-05-26 13:03:33 -0400

    Created project

---
+++ src/main/java/me/winter/grammar/japanese/package-info.kt
@@ -0,0 +1,7 @@
+/**
+ * Package dedictated to the japanese grammar of the application
+ *
+ * Created by Alexander Winter on 2017-05-26.
+ */
+package me.winter.grammar.japanese
+
---
+++ src/test/java/me/winter/grammar/test/package-info.kt
@@ -0,0 +1,7 @@
+/**
+ * Package dedicated to tests general functionnalities of the application
+ *
+ *
+ * Created by Alexander Winter on 2017-05-26.
+ */
+package me.winter.grammar.test
